04d266516c393bd73b7236de29f07b9b2221ab87
jani-123 <janina.rqa@gmail.com>
2017-08-03 14:32:05 -0500

    tablero

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+var generar = document.getElementById('genera');
+var tablero = document.getElementById('tablero');
+
+
+generar.onclick= function(){
+  var tabla=document.createElement('table');
+  tabla.border="1";
+  var nlados=document.getElementById('lados').value;
+  for(var i = 0; i < nlados; i++){
+    var fila= document.createElement('tr');
+    for (var j = 0; j < nlados; j++) {
+      var celda = document.createElement('td');
+      if(i%2==0&&j%2!=0||i%2!=0&&j%2==0){
+        celda.setAttribute('class','negro');
+      }
+      fila.appendChild(celda);
+    }
+    tabla.appendChild(fila);
+  }
+  tablero.appendChild(tabla);
+}
